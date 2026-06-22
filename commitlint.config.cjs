e8c9ea49a1dd590300305cade8f682bfc9a4b154
Wayne <chenzhenhui.wayne@gmail.com>
2026-06-22 16:28:51 +0800

    chore: add data commit type

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,3 +1,23 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'data',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+      ],
+    ],
+  },
 };
